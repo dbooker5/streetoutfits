89860cd7120559392200fab6397a5b1fba702dc0
dbooker5 <bookerotieno21@gmail.com>
2025-12-03 14:20:42 +0300

    feat: Initialize Drizzle ORM with core schemas for users, products, and authentication, including guest session management.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,46 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
+import { v4 as uuidv4 } from "uuid";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
+        schema: {
+            user: "user",
+            session: "session",
+            account: "account",
+            verification: "verification",
+        }
     }),
     emailAndPassword: {
         enabled: true,
+        requireEmailVerification: false,    
     },
+    socialProviders:{},
+    sessions:{
+        cookieCache: {
+            enabled: true,
+            maxAge: 60 * 60 * 24 * 7,
+        }
+    },
+    cookies: {
+        sessionToken: {
+         name:"auth_session",
+         options: {
+            httpOnly: true,
+            sameSite: "strict",
+            path: "/",
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 60 * 60 * 24 * 7,
+         }   
+        },
+    },
+    advanced: {
+        database: {
+            generateId: () => uuidv4(),
+        }
+    },
+    plugins: [nextCookies()],
 });
