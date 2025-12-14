97287043228c34aa8d3ea95ced43b583f90f4d63
dbooker5 <bookerotieno21@gmail.com>
2025-12-14 23:08:39 +0300

    feat: Implement initial Drizzle ORM schema with authentication and product tables, and add `lib/auth.ts` for authentication setup.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,4 @@
-import { betterAuth } from "better-auth";
+import { betterAuth, BetterAuthOptions } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
 import { v4 as uuidv4 } from "uuid";
@@ -19,28 +19,29 @@ export const auth = betterAuth({
         requireEmailVerification: false,    
     },
     socialProviders:{},
-    sessions:{
+    session:{
         cookieCache: {
             enabled: true,
             maxAge: 60 * 60 * 24 * 7,
         }
     },
-    cookies: {
-        sessionToken: {
-         name:"auth_session",
-         options: {
-            httpOnly: true,
-            sameSite: "strict",
-            path: "/",
-            secure: process.env.NODE_ENV === "production",
-            maxAge: 60 * 60 * 24 * 7,
-         }   
-        },
-    },
+
     advanced: {
+        cookies: {
+            sessionToken: {
+                name: "auth_session",
+                attributes: {
+                    httpOnly: true,
+                    sameSite: "strict",
+                    path: "/",
+                    secure: process.env.NODE_ENV === "production",
+                    maxAge: 60 * 60 * 24 * 7,
+                }
+            },
+        },
         database: {
             generateId: () => uuidv4(),
         }
     },
     plugins: [nextCookies()],
-});
+} satisfies BetterAuthOptions);
